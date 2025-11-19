31193842092a873b3673faf5ba108287eb868509
Zayn <zyh_zayn@outlook.com>
2025-11-19 20:49:29 +0800

    feat: 修改 start_qemu 脚本以在qemu上启动 linux-5.15

--- scripts/start_qemu.sh
+++ scripts/start_qemu.sh
@@ -1,6 +1,7 @@
 qemu-system-arm -M vexpress-a9 -m 1024M -smp 4 \
-  -kernel /home/zayn/project/Embedded-System-Design/assets/zImage \
-  -append "root=/dev/mmcblk1 rw rootfstype=ext3 init=/linuxrc console=ttyAMA0 ip=10.0.0.2:10.0.0.1:10.0.0.1:255.255.255.0" \
-  -drive if=sd,file=/home/zayn/project/Embedded-System-Design/assets/a9rootfs.ext3,format=raw \
+  -kernel /home/zayn/project/Embedded-System-Design/assets/zImage_5.15 \
+  -dtb /home/zayn/project/Embedded-System-Design/assets/vexpress-v2p-ca9.dtb \
+  -append "root=/dev/mmcblk0 rw rootfstype=ext4 init=/linuxrc console=ttyAMA0 ip=10.0.0.2:10.0.0.1:10.0.0.1:255.255.255.0" \
+  -drive if=sd,file=/home/zayn/project/Embedded-System-Design/assets/rootfs.ext4,format=raw \
   -net nic -net tap,ifname=tap0,script=no,downscript=no \
   -nographic
