29fff4908cb4f7d62031b5600c277cb6fbad359c
Zayn <zyh_zayn@outlook.com>
2025-12-17 19:19:16 +0800

    feat(webui): 修改 vite 配置和 main.ts

--- webui/vite.config.ts
+++ webui/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',
+  server: {
+    proxy: {
+      '/cgi-bin': {
+        target: 'http://127.0.0.1:8080',
+        changeOrigin: true,
+      },
+    },
+  },
 })
