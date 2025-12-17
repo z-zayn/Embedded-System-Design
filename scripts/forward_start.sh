d96bc87a59254702a642a8a9f9a1c647dfa0f9d8
Zayn <zyh_zayn@outlook.com>
2025-12-17 16:26:27 +0800

    feat: 现有脚本添加可行性权限

--- scripts/forward_start.sh
+++ scripts/forward_start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-nohup socat TCP-LISTEN:8080,fork,reuseaddr TCP:10.0.0.2:80  >/tmp/socat_web.log 2>&1 &
+nohup socat TCP-LISTEN:8080,fork,reuseaddr TCP:10.0.0.2:8081  >/tmp/socat_web.log 2>&1 &
 nohup socat TCP-LISTEN:9000,fork,reuseaddr TCP:10.0.0.2:9000 >/tmp/socat_file.log 2>&1 &
 echo "Forwarding: http://localhost:8080  and  tcp://localhost:9000"
